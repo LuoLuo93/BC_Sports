d74d21002705996bad5246ab44716614e5313f55
LuoLuo93 <1143775436@qq.com>
2026-06-08 17:42:20 +0800

    fix: upgrade Knife4j to 4.5.0 for Spring Boot 3.x compatibility

--- project/src/main/java/com/bcsport/admin/config/Knife4jConfig.java
+++ project/src/main/java/com/bcsport/admin/config/Knife4jConfig.java
@@ -1,72 +1,34 @@
 package com.bcsport.admin.config;
 
-import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.Contact;
-import springfox.documentation.service.SecurityReference;
-import springfox.documentation.service.SecurityScheme;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
-@EnableSwagger2
-@EnableKnife4j
 public class Knife4jConfig {
 
     @Bean
-    public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.bcsport.admin.controller"))
-                .paths(PathSelectors.any())
-                .build()
-                .securitySchemes(securitySchemes())
-                .securityContexts(securityContexts());
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("BC体育后台管理系统 API文档")
-                .description("BC体育后台管理系统接口文档，提供用户管理、角色管理、菜单权限等功能接口")
-                .contact(new Contact("BC体育技术部", "https://www.bcsport.com", "tech@bcsport.com"))
-                .version("1.0.0")
-                .build();
-    }
-
-    private List<SecurityScheme> securitySchemes() {
-        List<SecurityScheme> apiKeys = new ArrayList<>();
-        apiKeys.add(new ApiKey("Authorization", "Authorization", "header"));
-        return apiKeys;
-    }
-
-    private List<SecurityContext> securityContexts() {
-        List<SecurityContext> securityContexts = new ArrayList<>();
-        securityContexts.add(SecurityContext.builder()
-                .securityReferences(defaultAuth())
-                .operationSelector(o -> o.requestMappingPattern().startsWith("/api/"))
-                .build());
-        return securityContexts;
-    }
-
-    private List<SecurityReference> defaultAuth() {
-        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = authorizationScope;
-        List<SecurityReference> securityReferences = new ArrayList<>();
-        securityReferences.add(new SecurityReference("Authorization", authorizationScopes));
-        return securityReferences;
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("BC体育后台管理系统 API文档")
+                        .description("BC体育后台管理系统接口文档，提供用户管理、角色管理、菜单权限等功能接口")
+                        .contact(new Contact()
+                                .name("BC体育技术部")
+                                .url("https://www.bcsport.com")
+                                .email("tech@bcsport.com"))
+                        .version("1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList("Authorization"))
+                .components(new Components()
+                        .addSecuritySchemes("Authorization",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.APIKEY)
+                                        .name("Authorization")
+                                        .in(SecurityScheme.In.HEADER)));
     }
 }
